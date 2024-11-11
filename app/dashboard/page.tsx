a08459d4b377ea419ebe6daa4a26b385ec7abe2b
GlennMones19 <leoglennmones0@gmail.com>
2024-11-11 14:40:32 +0800

    Add user api to fetch the user and user messages

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,16 +1,81 @@
-import React from 'react'
-import LeftSide from './left_side';
+"use client";
+import { useState, useEffect } from "react";
+import Image from "next/image";
+import { Input } from "@/components/ui/input";
+import { ScrollArea } from "@/components/ui/scroll-area";
 import RightSide from './right_side';
 
+interface User {
+    id: number;
+    name: string;
+    email: string;
+    status: string;
+    messages: { id: number, message_body: string }[];
+}
+
 export default function Dashboard() {
+    const [showRight, setShowRight] = useState(false);
+    const [userData, setUserData] = useState<User[]>([]);
+    const [fetchserMessage, setfetchserMessage] = useState([]);
+
+    useEffect(() => {
+        fetchUsers();
+    }, []);
+
+    const fetchUsers = async () => {
+        const result = await fetch('http://127.0.0.1:8000/api/users');
+        const data: User[] = await result.json();
+        setUserData(data.users);
+    };
+
+    const fetchUserMessageById = async (id: number) => {
+        const result = await fetch(`http://127.0.0.1:8000/api/show_user/${id}`);
+        const data: User = await result.json();
+        setfetchserMessage(data.user);
+        setShowRight(true);
+    };
+
     return (
         <div className="flex">
             <div className="w-2/6 bg-[#16161a] h-screen p-10">
-                <LeftSide />
-            </div>
+                <div className="flex justify-between items-center text-[#f5f5f5]">
+                    <h1 className="text-2xl">Chats</h1>
+                    <Image src='/icons/new_message.svg' width={20} height={20} alt="New Message Icon" />
+                </div>
+                <div className="relative my-4">
+                    <span className="absolute inset-y-0 left-3 flex items-center pointer-events-none">
+                        <Image src="/icons/ic_outline-search.svg" width={24} height={24} alt="Search Icon" />
+                    </span>
+                    <Input
+                        placeholder="Search messages..."
+                        className="pl-10 py-5 bg-[#3a3b3c] rounded-full text-[#f5f5f5]"
+                    />
+                </div>
+                <ScrollArea className="h-[500px] w-auto rounded-md custom-scrollbar overflow-y-scroll">
+                    {userData.map((user) => (
+                        <div
+                            key={user.id}
+                            className="message-item mt-2 text-[#f5f5f5] cursor-pointer"
+                            onClick={() => fetchUserMessageById(user.id)}
+
+                        >
+                            <div className="flex items-center gap-5">
+                                <Image src="/images/profile.svg" width={45} height={45} alt="Profile Picture" />
+                                <div className="flex flex-col gap-1">
+                                    <p className="font-bold">{user.name}</p>
+                                    <p className="truncate max-w-xs text-sm">test</p>
+                                </div>
+                            </div>
+                            <p className="text-end mr-4">{user.status == 'Active Now' ? 'Now' : user.status}</p>
+                        </div>
+                    ))}
+                </ScrollArea>
+            </div >
             <div className="w-4/6 bg-[#3a3b3c] h-screen p-10">
-                <RightSide />
+                {showRight && fetchserMessage && (
+                    <RightSide user={fetchserMessage} />
+                )}
             </div>
-        </div>
-    )
+        </div >
+    );
 }
--- app/dashboard/right_side.tsx
+++ app/dashboard/right_side.tsx
@@ -1,19 +1,30 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import Image from 'next/image';
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { Textarea } from "@/components/ui/textarea";
 import { Button } from "@/components/ui/button";
 
+interface User {
+    id: number;
+    name: string;
+    email: string;
+    status: string;
+    messages: { id: number, message_body: string }[];
+}
 
-export default function RightSide() {
+interface RightSideProps {
+    user: User;
+}
+
+export default function RightSide({ user }: RightSideProps) {
     return (
         <div className="text-[#f5f5f5] h-full relative">
             <div className="flex justify-between gap-2">
                 <div className="flex gap-2">
                     <Image src="/images/profile.svg" width={45} height={45} alt='Profile Picture' />
                     <div className="flex flex-col gap-1">
-                        <p>John Doe</p>
-                        <p className="text-sm">Active Now</p>
+                        <p>{user.name}</p>
+                        <p className="text-sm">{user.status == "Active Now" ? "Now" : user.status}</p>
                     </div>
                 </div>
                 <div className="flex gap-8">
@@ -23,42 +34,16 @@ export default function RightSide() {
                 </div>
             </div>
             <div className="flex flex-col mt-8 relative from-zinc-50">
-                <ScrollArea className="h-[calc(100vh-150px)] w-auto rounded-md custom-scrollbar overflow-y-scroll pr-5 pb-7">
-                    <div className="sender-message flex gap-5">
-                        <Image src="/images/profile.svg" width={45} height={45} alt='Profile Picture' />
-                        <div className="w-3/5 h-11 rounded bg-[#f5f5f5]"></div>
-                    </div>
-                    <div className="sender-message flex justify-end gap-5 mt-6">
-                        <div className="w-2/5 h-11 rounded bg-[#f5f5f5]"></div>
-                    </div>
-                    <div className="sender-message flex gap-5 mt-6">
-                        <Image src="/images/profile.svg" width={45} height={45} alt='Profile Picture' />
-                        <div className="w-3/5 h-11 rounded bg-[#f5f5f5]"></div>
-                    </div>
-                    <div className="sender-message flex justify-end gap-5 mt-6">
-                        <div className="w-2/5 h-11 rounded bg-[#f5f5f5]"></div>
-                    </div>
-                    <div className="sender-message flex gap-5 mt-6">
-                        <Image src="/images/profile.svg" width={45} height={45} alt='Profile Picture' />
-                        <div className="w-3/5 h-11 rounded bg-[#f5f5f5]"></div>
-                    </div>
-                    <div className="sender-message flex justify-end gap-5 mt-6">
-                        <div className="w-2/5 h-11 rounded bg-[#f5f5f5]"></div>
-                    </div>
-                    <div className="sender-message flex gap-5 mt-6">
-                        <Image src="/images/profile.svg" width={45} height={45} alt='Profile Picture' />
-                        <div className="w-3/5 h-11 rounded bg-[#f5f5f5]"></div>
-                    </div>
-                    <div className="sender-message flex justify-end gap-5 mt-6">
-                        <div className="w-2/5 h-11 rounded bg-[#f5f5f5]"></div>
-                    </div>
-                    <div className="sender-message flex gap-5 mt-6">
-                        <Image src="/images/profile.svg" width={45} height={45} alt='Profile Picture' />
-                        <div className="w-3/5 h-11 rounded bg-[#f5f5f5]"></div>
-                    </div>
-                    <div className="sender-message flex justify-end gap-5 mt-6">
-                        <div className="w-2/5 h-11 rounded bg-[#f5f5f5]"></div>
-                    </div>
+                <ScrollArea className="h-[calc(100vh-150px)] w-auto rounded-md custom-scrollbar overflow-y-scroll pr-5 pb-20">
+                    {user.messages.map((msg) => (
+                        <div key={msg.id} className="sender-message flex mb-2 gap-5">
+                            <Image src="/images/profile.svg" width={45} height={45} alt='Profile Picture' />
+                            <div className="w-3/5 h-11 rounded bg-[#f5f5f5] text-base">
+                                <p className="h-full flex items-center flex-wrap ml-2">{msg.message_body}</p>
+                            </div>
+                        </div>
+                    ))}
+                    {/* Additional message content can be added here */}
                 </ScrollArea>
                 <div className="send-message flex justify-end items-center gap-2 absolute bottom-0 w-full p-2 bg-[#1f1f1f]">
                     <Textarea placeholder="Type your message here." className="bg-light rounded w-full" />
@@ -66,5 +51,5 @@ export default function RightSide() {
                 </div>
             </div>
         </div>
-    )
+    );
 }
